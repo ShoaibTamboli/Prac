d332742a55c01e228b453bdaf6afd863caf43ce6
ShoaibTamboli <shoaibtamboli03@gmail.com>
2021-11-14 12:33:14 +0530

    update: script added

---
+++ password checker/app.js
@@ -0,0 +1,41 @@
+let password=document.getElementById("password");
+let repassword=document.getElementById("repassword");
+let btn=document.getElementById("btn")
+let msg=document.getElementById("msg");
+let lengthmsg=document.getElementById("lengthmsg")
+
+
+password.addEventListener('input', passwordvalidate)
+
+
+function passwordvalidate() {
+var passlength=password.value.length;
+
+    if(passlength>10){
+        btn.disabled=false;
+        lengthmsg.innerText="Strong password"
+        lengthmsg.style.color="DarkGreen"
+    }else{
+        btn.disabled=true;
+        lengthmsg.innerText="password length must be atleast 10 characters long."
+        lengthmsg.style.color="Red"
+    }
+
+  
+}
+
+btn.addEventListener('click', validatepassword)
+
+function validatepassword() {
+    if (password.value===repassword.value) {
+        console.log("password matched");
+        msg.innerText="password matched";
+        msg.style.backgroundColor="green"
+
+    } else{
+       alert("password mis-matched")
+        msg.innerText="password mis-matched";
+        lengthmsg.style.visibility="hidden"
+        msg.style.backgroundColor="red"
+    }
+}
