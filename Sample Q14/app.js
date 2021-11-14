4c61eda55d3a79490131c18d8c9f31c41dc28e48
ShoaibTamboli <shoaibtamboli03@gmail.com>
2021-11-14 16:01:24 +0530

    update: Js added

---
+++ Sample Q14/app.js
@@ -0,0 +1,22 @@
+let user1={
+    name: "Ram",
+    age: "51", 
+     yuga: "Treta"
+}
+
+let user2={
+    name: "Krishna",
+    age: "31", 
+     yuga: "Dwapar" 
+}
+
+function result() {
+    if(user1.age>user2.age){
+        console.log(user1.name)
+    }
+    else{
+        console.log(user2.name) ;
+    }
+}
+
+result()
